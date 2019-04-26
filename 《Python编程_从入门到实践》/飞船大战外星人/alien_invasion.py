a97824f7a19b5b94de2ef9096017426120bea655
GitEndlessLearner <41732516+GitEndlessLearner@users.noreply.github.com>
2019-04-26 16:48:19 +0800

    Add files via upload

--- 《Python编程_从入门到实践》/飞船大战外星人/alien_invasion.py
+++ 《Python编程_从入门到实践》/飞船大战外星人/alien_invasion.py
@@ -6,6 +6,7 @@ import game_functions as gf
 from pygame.sprite import Group
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 def run_game():
 	# 初始化游戏并创建一个屏幕对象
@@ -14,6 +15,9 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("蔡徐坤大战外星人")
+
+	# 创建play按钮
+	play_button = Button(ai_settings, screen, "PLAY")
 
 	# 创建一个用于存储游戏统计信息的实例
 	stats = GameStats(ai_settings)
@@ -30,12 +34,14 @@ def run_game():
 	while  True:
 		
 		# 监视鼠标和键盘动作1
-		gf.check_events(ai_settings, screen, ship, bullets)
+		gf.check_events(ai_settings, screen, stats, play_button, ship,
+				aliens, bullets)
 		if stats.game_active:
 			ship.update()
 			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 	
-		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+			play_button)
 
 run_game()
